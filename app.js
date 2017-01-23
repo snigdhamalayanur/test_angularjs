6896352e37c6b32380aefb9387b3caa43321e35a
snigdha malayanur <snigdha malayanur>
2017-01-23 23:38:47 +0530

    Assignment submission

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-(function(){
+(function () {
 'use strict';
-angular.module('myFirstApp',[])
 
-.controller('myFirstController',function(){
+angular.module('LunchChecker', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  var Itemslist=$scope.Items;
+  $scope.Message='';
+$scope.CheckIfTooMuch = function(Itemslist){ 
+    if(!Itemslist )
+    {
+        $scope.Message="Please enter value first!!";
+    }
+else{
+    var ItemsArray = Itemslist.split(',');
+    var ItemsCount = ItemsArray.length;
+    
+    if(ItemsCount >= 1 && ItemsCount <= 3)
+   {
+    $scope.Message="Enjoy !!!";
+   }
+   else 
+   {
+       $scope.Message="Too Much!!";
+   }
+}
+    
+}
 
 
-
-});
-
-
-
+}
 
 })();
